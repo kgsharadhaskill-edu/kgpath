a9ede6527600fdec5bbcb98470c0c2e80fa3f7ad
kgsharadhaskill-edu <kg.sharadhaskill@gmail.com>
2025-11-13 13:18:37 +0530

    Your commit message here

--- admin/chatbot.php
+++ admin/chatbot.php
@@ -123,6 +123,7 @@ if (isset($_REQUEST['action'])) {
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Admin | Live Chat</title>
+    <link rel="icon" type="image/png" href="dist/img/favicon.png">
     <!-- Google Font: Source Sans Pro -->
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
     <!-- Font Awesome -->
